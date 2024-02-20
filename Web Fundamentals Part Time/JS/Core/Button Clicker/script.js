24755cdc2fe47312fa6167953f8287c16c31811b
unknown <baraahedri69@gmail.com>
2024-02-20 18:28:35 +0100

    button clicker

---
+++ Web Fundamentals Part Time/JS/Core/Button Clicker/script.js
@@ -0,0 +1,13 @@
+function login(button) {
+    if (button.textContent === "Login") {
+        button.textContent = "Logout";
+    }
+}
+
+function hide(button) {
+    button.style.display = "none";
+  }
+
+  function message() {
+    alert("Ninja was liked");
+}
